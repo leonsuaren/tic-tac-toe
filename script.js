53a93ce2af69d1aab95ffc6f1e96ee799c069c39
Leon Suarez <leonsua@gmail.com>
2022-05-22 17:29:39 -0400

    update the player and the X or O

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-const Square = ({ id, player }) => {
+const Square = ({ id, newState }) => {
   const [color, setColor] = React.useState('green');
+  const [status, setStatus] = React.useState(null);
+  let xo = ['O', 'X'];
+
   const palet = ['red', 'blue', 'green'];
   const getRandomColor = () => palet[Math.floor(Math.random()*3)];
   React.useEffect(() => {
     console.log(`Render ${id}`);
     return () => console.log(`unmounting square ${id}`)
   }, []);
-  
+
   return (
     <button
       onClick={(e) => {
-        setColor(getRandomColor)
-        e.target.style.background = color
+        let col = getRandomColor()
+        setColor(col)
+        let nextPlayer = newState({id: id, color: col})
+        setStatus(nextPlayer);
+        e.target.style.background = col
       }}
     >
-    <h1>{id}</h1>
+    <h1>{xo[status]}</h1>
     </button>
   )
 }
 
 const Board = () => {
   const [player, setPlayer] = React.useState(0);
-  const [mounted, setMounted] = React.useState(true);
+  const [state, setState] = React.useState([]);
   let status = `Player ${player}`;
+
+  const newState = (ob) => {
+    let nextPlayer = (player +1) % 2;
+    setPlayer(nextPlayer);
+    setState([...state, ob]);
+    console.log(`Adding state ${JSON.stringify(ob)}`);
+    status = `Player ${player}`;
+    return nextPlayer;
+  }
+
   function renderSquare(i) {
-    return <Square id={i} player={player}></Square>
+    return <Square id={i} player={player} newState={newState}></Square>
   }
-  const toggle = () => {
-    setMounted(!mounted)
-  }
+
   return (
     <div className='game-board'>
       <div className='grid-row'>
-        {mounted && renderSquare(0)}
-        {mounted && renderSquare(1)}
-        {mounted && renderSquare(2)}
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className='info'>
-        <button onClick={toggle}>Show/Hide Row</button>
+        <button>Show/Hide Row</button>
         <h1>{status}</h1>
       </div>
     </div>
